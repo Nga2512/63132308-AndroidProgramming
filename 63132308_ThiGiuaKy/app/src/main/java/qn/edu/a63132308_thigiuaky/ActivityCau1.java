94dc424ce92a81c7ac755206917eba93186bfd51
HuynhTranQuynhNga <nga.htq.63cntt@ntu.edu.vn>
2025-03-31 00:45:15 +0700

    Luyện tập GK

---
+++ 63132308_ThiGiuaKy/app/src/main/java/qn/edu/a63132308_thigiuaky/ActivityCau1.java
@@ -0,0 +1,27 @@
+package qn.edu.a63132308_thigiuaky;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityCau1 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cau1);
+//        EditText edtSoA = findViewById(R.id.edtSoA);
+//        EditText edtSoB = findViewById(R.id.edtSoB);
+//        Button btnTinhTong = findViewById(R.id.btnTinhTong);
+//        TextView tvKetQua = findViewById(R.id.tvKetQua);
+//
+//        btnTinhTong.setOnClickListener(v -> {
+//            int a = Integer.parseInt(edtSoA.getText().toString());
+//            int b = Integer.parseInt(edtSoB.getText().toString());
+//            int tong = a + b;
+//            tvKetQua.setText("Kết quả: " + tong);
+//        });
+    }
+}
